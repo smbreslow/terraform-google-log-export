deda8128d3825913fdab17c87d72653cd00a0694
smbreslow <29959205+smbreslow@users.noreply.github.com>
2019-12-09 16:18:45 -0500

    Update variables.tf

--- examples/datadog-sink/variables.tf
+++ examples/datadog-sink/variables.tf
@@ -28,3 +28,8 @@ variable "push_endpoint" {
   description = "The URL locating the endpoint to which messages should be pushed"
   type        = string
 }
+
+variable "key_output_path" {
+  description = "The path to a directory where the JSON private key of the new Datadog service account will be created"
+  type        = string
+}
