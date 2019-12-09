3ce0b254103e4106a1bcc550f0c59156b3028284
smbreslow <29959205+smbreslow@users.noreply.github.com>
2019-12-09 15:49:05 -0500

    Update outputs.tf

--- examples/datadog-sink/outputs.tf
+++ examples/datadog-sink/outputs.tf
@@ -26,12 +26,7 @@ output "pubsub_topic_project" {
 
 output "pubsub_subscription_name" {
   description = "Pub/Sub topic subscription name"
-  value       = module.destination.pubsub_subscription
-}
-
-output "pubsub_subscriber" {
-  description = "Pub/Sub topic subscriber email"
-  value       = module.destination.pubsub_subscriber
+  value       = module.destination.pubsub_push_subscription
 }
 
 output "datadog_service_account" {
