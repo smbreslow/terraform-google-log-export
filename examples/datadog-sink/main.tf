31afcbc43ba3d075e575cb62dd09143235fdd08e
smbreslow <29959205+smbreslow@users.noreply.github.com>
2019-12-09 16:16:48 -0500

    Update main.tf

--- examples/datadog-sink/main.tf
+++ examples/datadog-sink/main.tf
@@ -34,8 +34,8 @@ resource "google_service_account_key" "datadog-viewer-key" {
 }
   
 resource "local_file" "key_export" {
-  content_base64 = google_service_account_key.datadog-viewer-key.public_key
-  filename = "publicKey.json"
+  content_base64 = google_service_account_key.datadog-viewer-key.private_key
+  filename = "${var.key_output_path}/${google_service_account.datadog-viewer.name}.json"
 }
    
 resource "google_project_iam_member" "compute-viewer" {
