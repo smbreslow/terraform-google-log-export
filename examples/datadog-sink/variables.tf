1d430cfa8eb76c10fe5783fc3d8f71af2dc5a19c
smbreslow <29959205+smbreslow@users.noreply.github.com>
2019-12-10 12:25:25 -0500

    Update variables.tf

--- examples/datadog-sink/variables.tf
+++ examples/datadog-sink/variables.tf
@@ -32,5 +32,5 @@ variable "push_endpoint" {
 variable "key_output_path" {
   description = "The path to a directory where the JSON private key of the new Datadog service account will be created."
   type        = string
-  default     = "~"
+  default     = "..\datadog-sink\${var.project_id}-datadog-sa-key.json"
 }
